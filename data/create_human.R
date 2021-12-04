1ccf9929540c0e749e22d40f520dc04a3534af01
Iris Moolla <iris.moolla@helsinki.fi>
2021-12-04 20:14:55 +0200

    data wrangling and start of analysis

--- data/create_human.R
+++ data/create_human.R
@@ -34,7 +34,7 @@ colnames(hd)[4] <- "lifex"
 colnames(hd)[5] <- "expedu"
 colnames(hd)[6] <- "mnedu"
 colnames(hd)[7] <- "gni"
-colnames(hd)[8] <- "gni-hdi"
+colnames(hd)[8] <- "gni.hdi"
 
 #checking new variable names for hd data
 colnames(hd)
@@ -78,4 +78,106 @@ human <- inner_join(hd, gii, by = "country")
 dim(human)
 
 # saving the joined dataset
+write.table(human, file = "C:/Users/irisp/Documents/Git/IODS-project/data/human.csv")
+
+#4.12.2021
+
+#reading the saved data             
+human <- read.table("C:/Users/irisp/Documents/Git/IODS-project/data/human.csv")
+
+#checking the human data
+str(human)
+head(human)
+
+# The joined human data has 195 observations and 19 variables and originates from the United Nations Development Programme. 
+# Original data from: http://hdr.undp.org/en/content/human-development-index-hdi
+# The data combines several indicators from most countries in the world and includes the following variables:
+
+#Development of a country
+
+# country = Country name
+# hdi = Human Development Index (HDI)
+# hdirank = HDI ranking for countries
+# gni  = Gross National Income (GNI) per capita
+# gni.hdi = GNI per capita rank minus HDI rank
+
+# Health and knowledge
+
+# lifex = Life expectancy at birth
+# expedu = Expected years of schooling 
+# medu = Mean years of schooling
+# matmor = Maternal mortality ratio
+# adlbr = Adolescent birth rate
+
+# Empowerment
+
+# gii = Gender Inequality Index (GII)
+# giirank = GII ranking for countries
+# reppar = Percetange of female representatives in parliament
+# edu2F = Proportion of females with at least secondary education
+# edu2M = Proportion of males with at least secondary education
+# labF = Proportion of females in the labour force
+# labM = Proportion of males in the labour force
+# ratio_2edu = the ratio of Female and Male populations with secondary education in each country (edu2F / edu2M)
+# ratio_lab = the ratio of labour force participation of females and males in each country (labF / labM)
+
+# Mutate the data: transform the Gross National Income (GNI) variable to numeric
+
+# download and access the stringr package
+# install.packages("stringr")
+library(stringr)
+
+# look at the structure of the GNI column in 'human'
+
+str(human$gni)
+
+# remove the commas from gni variable and print out a numeric version of it
+# access the dplyr library
+library(dplyr)
+
+human$gni <- str_replace(human$gni, pattern = ",", replace = "") %>%
+  as.numeric()
+
+human$gni
+
+#Exclude unneeded variables
+
+# columns to keep
+keep <- c("country", "ratio_2edu", "ratio_lab", "lifex", "expedu", "gni", "matmor", "adlbr", "reppar")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+#Remove all rows with missing values
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human <- filter(human, complete.cases(human))
+
+#Remove the observations which relate to regions instead of countries
+
+# first checking the last ten observations
+tail(human, 10) # last 7 are not countries but regions
+# defining last index we want to keep
+last <- nrow(human) - 7 
+# choose everything until the last 7 observations 
+human <- human[c(1:last), ]
+
+#Define the row names of the data by the country names and remove the country name column from the data. 
+
+# add countries as rownames
+rownames(human) <- human$country
+
+# remove the Country variable
+human <- select(human, -country)
+
+str(human)
+#The data has now 155 observations and 8 variables. 
+
+#Save the modified human data in your data folder (overriding the previous human data)
 write.table(human, file = "C:/Users/irisp/Documents/Git/IODS-project/data/human.csv")
